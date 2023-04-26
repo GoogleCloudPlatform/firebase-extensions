6951818649cfb639ee85db08a4135dcdd6865ad7
Darren Ackers <ackers86@hotmail.com>
2023-04-26 14:40:38 +0100

    feat: added api key diagnostic

---
+++ api-keys-diagnostic/PREINSTALL.md
@@ -0,0 +1,26 @@
+The API Keys Diagnostic Extension helps you identify unsecured API keys in your Firebase project by regularly scanning your project's API keys and sending notifications if any unrestricted keys are found. 
+
+This extension uses Cloud Functions to interact with the Google Cloud API and [Eventarc](https://cloud.google.com/eventarc/docs) to allow developers to subscribe to the discovery of any unrestricted keys.
+
+### Schedule scanning
+
+This extension regularly scans your project's API keys: Cloud Function are scheudled tp run every 2 minutes, checking your project's API keys for any missing restrictions.
+
+The Cloud Function filters the API keys to find any unrestricted keys and logs them.
+
+If any unsecured API keys are found, the extension sends an event via the Eventarc channel.### 
+
+### Pre-requisites
+
+You need to set up a Eventarc function listener to subscribe to any unrestricted keys, and to handle the events accordingly. 
+
+### Billing
+
+To install an extension, your project must be on the Blaze (pay as you go) plan
+
+You will be charged a small amount (typically around $0.01/month) for the Firebase resources required by this extension (even if it is not used).
+
+This extension uses other Firebase and Google Cloud Platform services, which have associated charges if you exceed the service’s no-cost tier:
+Google Cloud API
+Eventarc
+Cloud Functions (Node.js 10+ runtime. See FAQs)
---
+++ api-keys-diagnostic/functions/src/index.ts
@@ -0,0 +1,55 @@
+import * as functions from "firebase-functions";
+import { apikeys_v2, google } from "googleapis";
+import * as admin from "firebase-admin";
+import { getEventarc } from "firebase-admin/eventarc";
+import config from "./config";
+
+admin.initializeApp();
+
+const eventChannel =
+  process.env.EVENTARC_CHANNEL &&
+  getEventarc().channel(process.env.EVENTARC_CHANNEL);
+
+export const apiKeysDiagnostic = functions.pubsub
+  .schedule("every 2 minutes")
+  .onRun(async () => {
+    try {
+      const auth = new google.auth.GoogleAuth({
+        scopes: "https://www.googleapis.com/auth/cloud-platform",
+      });
+
+      const authClient = await auth.getClient();
+
+      const client = new apikeys_v2.Apikeys({
+        auth: authClient,
+      });
+
+      const { data } = await client.projects.locations.keys.list({
+        parent: `projects/${config.projectId}/locations/global`,
+      });
+
+      if (!data.keys) return;
+
+      // Check if any of the API keys are not restricted
+      const unsecuredKeys = data.keys.filter((key) => !key.restrictions);
+      functions.logger.log(JSON.stringify(unsecuredKeys, null, 2));
+
+      // If there are any unsecured keys, send an email notification
+      if (eventChannel && unsecuredKeys.length > 0) {
+        functions.logger.info("Sending event");
+
+        await eventChannel.publish({
+          type: "firebase.extensions.api-keys-diagnostic.unsecured",
+          subject: "Unsecured API Keys Found",
+          data: {
+            message: `The following API keys have no restrictions:\n\n${unsecuredKeys
+              .map((key) => key.name)
+              .join("\n")}`,
+            keys: JSON.stringify(unsecuredKeys),
+          },
+        });
+      }
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  });
