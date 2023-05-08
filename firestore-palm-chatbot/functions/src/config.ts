2401e818990e01d4ba9c211a8000ea34ac70d561
Jacob Cable <jacobcable94@gmail.com>
2023-05-08 22:48:37 +0100

    feat: adding Chatbot with PaLM API

---
+++ firestore-palm-chatbot/functions/src/config.ts
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export interface Config {
+  model: string;
+  context?: string;
+  promptField: string;
+  responseField: string;
+  orderField: string;
+  enableDiscussionOptionOverrides: boolean;
+  collectionName: string;
+  temperature?: number;
+  topP?: number;
+  topK?: number;
+  candidateCount?: number;
+  candidatesField?: string;
+}
+
+const config: Config = {
+  collectionName:
+    process.env.COLLECTION_NAME ||
+    "users/{uid}/discussions/{discussionId}/messages",
+  model: process.env.MODEL || "models/chat-bison-001",
+  context: process.env.CONTEXT,
+  promptField: process.env.PROMPT_FIELD || "prompt",
+  responseField: process.env.RESPONSE_FIELD || "response",
+  orderField: process.env.ORDER_FIELD || "createTime",
+  enableDiscussionOptionOverrides:
+    process.env.ENABLE_DISCUSSION_OPTION_OVERRIDES === "yes",
+  temperature: process.env.TEMPERATURE
+    ? parseFloat(process.env.TEMPERATURE)
+    : undefined,
+  topP: process.env.TOP_P ? parseFloat(process.env.TOP_P) : undefined,
+  topK: process.env.TOP_K ? parseInt(process.env.TOP_K) : undefined,
+  candidateCount: process.env.CANDIDATE_COUNT
+    ? parseInt(process.env.CANDIDATE_COUNT)
+    : 1,
+  candidatesField: process.env.CANDIDATES_FIELD || "candidates",
+};
+
+export default config;
