65015d2ea9bb8f01333800e2718a02d7c24120a2
Darren Ackers <ackers86@hotmail.com>
2023-09-05 10:16:58 +0100

    chore(speech-to-text): updated emulator host and dependencies

--- speech-to-text/functions/__tests__/transcribe-audio.test.ts
+++ speech-to-text/functions/__tests__/transcribe-audio.test.ts
@@ -6,13 +6,13 @@ import {Bucket} from '@google-cloud/storage';
 import {uploadTranscodedFile} from '../src/transcribe-audio';
 import {Status, UploadAudioResult} from '../src/types';
 
-process.env.FIREBASE_STORAGE_EMULATOR_HOST = 'localhost:9199';
-process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
-process.env.FIREBASE_FIRESTORE_EMULATOR_ADDRESS = 'localhost:8080';
-process.env.FIREBASE_AUTH_EMULATOR_HOST = 'localhost:9099';
-process.env.PUBSUB_EMULATOR_HOST = 'localhost:8085';
+process.env.FIREBASE_STORAGE_EMULATOR_HOST = '127.0.0.1:9199';
+process.env.FIRESTORE_EMULATOR_HOST = '127.0.0.1:8080';
+process.env.FIREBASE_FIRESTORE_EMULATOR_ADDRESS = '127.0.0.1:8080';
+process.env.FIREBASE_AUTH_EMULATOR_HOST = '127.0.0.1:9099';
+process.env.PUBSUB_EMULATOR_HOST = '127.0.0.1:8085';
 process.env.GOOGLE_CLOUD_PROJECT = 'demo-test';
-process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
+process.env.FIRESTORE_EMULATOR_HOST = '127.0.0.1:8080';
 
 admin.initializeApp({projectId: 'demo-test'});
 
