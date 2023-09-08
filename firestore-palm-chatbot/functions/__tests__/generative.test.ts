06e1b19d82901656d991df0f4a2a3a37e84264b5
Darren Ackers <ackers86@hotmail.com>
2023-09-08 10:10:38 +0100

    chore(firestore-palm-chatbot): updated observer checks and resetting mocks

--- firestore-palm-chatbot/functions/__tests__/generative.test.ts
+++ firestore-palm-chatbot/functions/__tests__/generative.test.ts
@@ -70,7 +70,7 @@ const wrappedGenerateMessage = fft.wrap(
   generateMessage
 ) as WrappedFirebaseFunction;
 
-const firestoreObserver = jest.fn();
+const firestoreObserver = jest.fn(() => {});
 let collectionName: string;
 
 describe('generateMessage', () => {
@@ -78,28 +78,34 @@ describe('generateMessage', () => {
 
   // clear firestore
   beforeEach(async () => {
+    await fetch(
+      `http://${process.env.FIRESTORE_EMULATOR_HOST}/emulator/v1/projects/demo-gcp/databases/(default)/documents`,
+      {method: 'DELETE'}
+    );
+    jest.clearAllMocks();
     const randomInteger = Math.floor(Math.random() * 1000000);
     collectionName = config.collectionName.replace(
       '{discussionId}',
       randomInteger.toString()
     );
-    jest.clearAllMocks();
-    await fetch(
-      `http://${process.env.FIRESTORE_EMULATOR_HOST}/emulator/v1/projects/dev-extensions-testing/databases/(default)/documents`,
-      {method: 'DELETE'}
-    );
+
     // set up observer on collection
     unsubscribe = admin
       .firestore()
       .collection(collectionName)
       .onSnapshot(snap => {
-        firestoreObserver(snap);
+        /** There is a bug on first init and write, causing the the emulator to the observer is called twice
+         * A snapshot is registered on the first run, this affects the observer count
+         * This is a workaround to ensure the observer is only called when it should be
+         */
+        if (snap.docs.length) firestoreObserver(snap);
       });
   });
   afterEach(() => {
     if (unsubscribe && typeof unsubscribe === 'function') {
       unsubscribe();
     }
+    jest.clearAllMocks();
   });
 
   test('should not run if the prompt field is not set', async () => {
@@ -398,7 +404,7 @@ const simulateFunctionTriggered =
   };
 
 const expectNoOp = async () => {
-  await new Promise(resolve => setTimeout(resolve, 500));
+  await new Promise(resolve => setTimeout(resolve, 100));
   expect(firestoreObserver).toHaveBeenCalledTimes(1);
   expect(mockAPI).toHaveBeenCalledTimes(0);
 };
