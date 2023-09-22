c6490798d46fd9f187607451424204201744f54e
Jacob Cable <jacobcable94@gmail.com>
2023-09-22 17:06:33 +0100

    feat(bigquery-firestore-export): handle datatypes

--- bigquery-firestore-export/functions/pipeline/src/main/java/com/pipeline/ExportPipeline.java
+++ bigquery-firestore-export/functions/pipeline/src/main/java/com/pipeline/ExportPipeline.java
@@ -5,6 +5,7 @@ import com.google.api.services.bigquery.model.TableRow;
 import com.google.api.services.bigquery.model.TableSchema;
 import com.google.cloud.firestore.FirestoreOptions;
 import com.google.firestore.v1.Document;
+import com.google.firestore.v1.MapValue;
 import com.google.firestore.v1.Value;
 import com.google.firestore.v1.Write;
 import com.google.protobuf.ByteString;
@@ -29,6 +30,7 @@ import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.SerializableFunction;
+import java.time.Instant;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
@@ -102,7 +104,7 @@ public class ExportPipeline {
         .apply("ReadFromBigQuery",
             BigQueryIO.read(new SerializableFunction<SchemaAndRecord, Map<String, Value>>() {
               public Map<String, Value> apply(SchemaAndRecord schemaAndRecord) {
-                return convertToFirestoreValue(schemaAndRecord);
+                return convertToFirestoreValue(schemaAndRecord.getRecord());
               }
             }).withoutValidation().fromQuery(options.getQuery()).usingStandardSql()
                 .withTemplateCompatibility())
@@ -134,76 +136,79 @@ public class ExportPipeline {
     return documentPath + "/" + path;
   }
 
-  private static Map<String, Value> convertToFirestoreValue(SchemaAndRecord schemaAndRecord) {
+  private static Map<String, Value> convertToFirestoreValue(GenericRecord record) {
     Map<String, Value> firestoreDocument = new HashMap<>();
 
-    GenericRecord record = schemaAndRecord.getRecord();
+    // from the BQ source code comments:
+    // * [Required] The field data type. Possible values include STRING, BYTES,
+    // INTEGER, INT64 (same as
+    // * INTEGER), FLOAT, FLOAT64 (same as FLOAT), NUMERIC, BIGNUMERIC, BOOLEAN,
+    // BOOL (same as BOOLEAN),
+    // * TIMESTAMP, DATE, TIME, DATETIME, INTERVAL, RECORD (where RECORD indicates
+    // that the field
+    // * contains a nested schema) or STRUCT (same as RECORD).
 
     record.getSchema().getFields().forEach(field -> {
       Object fieldValue = record.get(field.pos());
 
-      // from the BQ source code comments:
-      // * [Required] The field data type. Possible values include STRING, BYTES,
-      // INTEGER, INT64 (same as
-      // * INTEGER), FLOAT, FLOAT64 (same as FLOAT), NUMERIC, BIGNUMERIC, BOOLEAN,
-      // BOOL (same as BOOLEAN),
-      // * TIMESTAMP, DATE, TIME, DATETIME, INTERVAL, RECORD (where RECORD indicates
-      // that the field
-      // * contains a nested schema) or STRUCT (same as RECORD).
       String fieldType = field.schema().getType().getName();
 
       Value.Builder valueBuilder = Value.newBuilder();
+      try {
+        switch (fieldType) {
+          case "STRING":
+            valueBuilder.setStringValue((String) fieldValue);
+            break;
+          case "BYTES":
+            ByteString byteString = ByteString.copyFrom((byte[]) fieldValue);
+            valueBuilder.setBytesValue(byteString);
+            break;
+          case "INTEGER":
+          case "INT64":
+            valueBuilder.setIntegerValue((long) fieldValue);
+            break;
+          case "FLOAT":
+          case "FLOAT64":
+            valueBuilder.setDoubleValue((double) fieldValue);
+            break;
+          case "BOOLEAN":
+          case "BOOL":
+            String originalValue = fieldValue.toString();
 
-      switch (fieldType) {
-        case "STRING":
-          fieldValue = fieldValue.toString();
-          valueBuilder.setStringValue(fieldValue.toString());
-          break;
-        case "BYTES":
-          ByteString byteString = ByteString.copyFrom((byte[]) fieldValue);
-          valueBuilder.setBytesValue(byteString);
-          break;
-        case "INTEGER":
-        case "INT64":
-          valueBuilder.setIntegerValue(Long.parseLong(fieldValue.toString()));
-          break;
-        case "FLOAT":
-        case "FLOAT64":
-          valueBuilder.setDoubleValue(Double.parseDouble(fieldValue.toString()));
-          break;
-        // case "NUMERIC":
-        // case "BIGNUMERIC":
-        // break;
-        case "BOOLEAN":
-        case "BOOL":
-          fieldValue = Boolean.parseBoolean(fieldValue.toString());
-          break;
-        // case "TIMESTAMP":
-        // TODO: handle timestamps
-        // Timestamp timestamp =
-        // Timestamp.newBuilder().setSeconds(Long.parseLong(fieldValue.toString())).build();
-        // valueBuilder.setTimestampValue(fieldValue.toString());
-        // break;
-        case "DATE":
-        case "TIME":
-        case "DATETIME":
-          fieldValue = fieldValue.toString();
-          break;
-        case "INTERVAL":
-          fieldValue = fieldValue.toString();
-          break;
-        case "RECORD":
-        case "STRUCT":
-        // TODO: We need to recursively convert the nested records
-          fieldValue = fieldValue.toString();
-          break;
-        default:
-          valueBuilder.setStringValue(fieldValue.toString());
-          break;
+            if (originalValue.equals("true")) {
+              valueBuilder.setBooleanValue(true);
+            } else {
+              valueBuilder.setBooleanValue(false);
+            }
+            break;
+          case "TIMESTAMP":
+            Instant instant = Instant.parse(fieldValue.toString());
+            long epochSecond = instant.getEpochSecond();
+            int nanoSecond = instant.getNano();
 
+            Timestamp timestamp = Timestamp.newBuilder().setSeconds(epochSecond).setNanos(nanoSecond).build();
+            valueBuilder.setTimestampValue(timestamp);
+            break;
+            // TODO: Add support for the following types. For now we just dump them in as strings.
+          // case "DATE":
+          // case "TIME":
+          // case "DATETIME":
+          // case "INTERVAL":
+          case "RECORD":
+          case "STRUCT":
+            GenericRecord structRecord = (GenericRecord) fieldValue;
+            Map<String, Value> nestedMap = convertToFirestoreValue(structRecord);
+            valueBuilder.setMapValue(MapValue.newBuilder().putAllFields(nestedMap));
+            break;
+          default:
+            valueBuilder.setStringValue(fieldValue.toString());
+            break;
+        }
+      } catch (Exception e) {
+        valueBuilder.setStringValue(fieldValue.toString());
       }
-      firestoreDocument.put(field.name(), valueBuilder.build());
 
+      firestoreDocument.put(field.name(), valueBuilder.build());
     });
 
     return firestoreDocument;
