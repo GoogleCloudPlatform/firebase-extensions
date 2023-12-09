8caba80c1f3bd73e89ce98dbe63d2086463c8ca6
Jacob Cable <jacobcable94@gmail.com>
2023-12-09 16:28:04 +0000

    test(firestore-palm-chatbot): add some tests for new override logic

--- firestore-palm-chatbot/functions/src/firestore.ts
+++ firestore-palm-chatbot/functions/src/firestore.ts
@@ -1,6 +1,8 @@
 import config from './config';
 import {Message, GenerateMessageOptions} from './types';
 import {DocumentSnapshot, DocumentReference} from 'firebase-admin/firestore';
+import {z} from 'zod';
+import {extractOverrides} from './overrides';
 
 /** Utils for extracting conversation info from firestore */
 
@@ -33,8 +35,6 @@ export async function fetchDiscussionOptions(
 
   const discussionDocSnap = await discussionDocRef.get();
 
-  console.log('discussionDocSnap', discussionDocSnap.data());
-
   if (!discussionDocSnap.exists) return {};
 
   const overrides = extractOverrides(discussionDocSnap);
@@ -56,45 +56,6 @@ export async function fetchDiscussionOptions(
   }
 
   return overrides;
-}
-import {z} from 'zod';
-
-const stringSchema = z.string();
-const intSchema = z.union([
-  z.string().transform(arg => parseInt(arg)),
-  z.number(),
-]);
-const floatSchema = z.union([z.string().transform(parseFloat), z.number()]);
-
-function extractOverrides(docSnap: DocumentSnapshot): Record<string, unknown> {
-  const overrides: Record<string, unknown> = {};
-
-  const stringFields = ['context', 'model'];
-  const intFields = ['topK', 'candidateCount'];
-  const floatFields = ['topP', 'temperature'];
-
-  extractOverridesByType(stringFields, docSnap, overrides, stringSchema);
-  extractOverridesByType(intFields, docSnap, overrides, intSchema);
-  extractOverridesByType(floatFields, docSnap, overrides, floatSchema);
-
-  return overrides;
-}
-
-function extractOverridesByType(
-  fields: string[],
-  docSnap: DocumentSnapshot,
-  overrides: Record<string, unknown>,
-  schema: z.ZodType<any, any, any>
-): void {
-  fields.forEach(field => {
-    const rawValue = docSnap.get(field);
-    try {
-      const value = schema.parse(rawValue);
-      overrides[field] = value;
-    } catch (error) {
-      // TODO: add warning
-    }
-  });
 }
 
 function validateExamples(examples: Record<string, unknown>[]): Message[] {
