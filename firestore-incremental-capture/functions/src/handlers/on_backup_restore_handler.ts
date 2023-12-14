819d8ab1ad883b61f41ef6fda69987512b73207c
Mais <mais.alheraki@gmail.com>
2023-12-14 16:08:25 +0300

    chore: license header

--- firestore-incremental-capture/functions/src/handlers/on_backup_restore_handler.ts
+++ firestore-incremental-capture/functions/src/handlers/on_backup_restore_handler.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {logger} from 'firebase-functions/v1';
 
 import {launchJob} from '../common';
@@ -14,90 +30,11 @@ export const onBackupRestoreHandler = async (data: any) => {
 
   logger.info(`Running backup restoration at PIT ${timestamp}`);
 
-  // const importDoc = await db
-  //   .doc(config.backupDoc)
-  //   .collection('imports')
-  //   .add({});
-
-  // // Get the latest backup collection
-  // const backupExportsCollection = db
-  //   .doc(config.backupDoc)
-  //   .collection('exports');
-
-  // const completedExports = await backupExportsCollection
-  //   .where('status', '==', 'Completed')
-  //   .get();
-
-  // const documents = completedExports.docs.map(doc => ({
-  //   id: doc.id,
-  //   data: doc.data(),
-  // }));
-
-  // // Sort documents by timestamp in descending order
-  // const sortedDocuments = documents.sort(
-  //   (a, b) => b.data.timestamp.toDate() - a.data.timestamp.toDate()
-  // );
-
-  // Get the most recent document
-  // const doc = sortedDocuments.length > 0 ? sortedDocuments[0] : null;
-
-  //TODO: use this version in the future, index creation is needed.
-  // const backupDocuments = await db
-  //   .doc(config.backupDoc)
-  //   .collection('exports')
-  //   .where('status', '==', 'Completed')
-  //   .orderBy('timestamp', 'desc')
-  //   .limit(1)
-  //   .get();
-
-  // Get the latest backup
-  // const backupId = doc?.id;
-
-  // // If no backup
-  // if (!backupId) {
-  //   logger.info('No backup found');
-  //   return Promise.resolve();
-  // }
-
   try {
-    // // Export the Firestore db to storage
-    // const {id, operation} = await createImport(backupId);
-
-    // // Update Firestore for tracking
-    // await importDoc.set({
-    //   id,
-    //   status: 'Running import...',
-    //   operation: operation.name,
-    //   timestamp: FieldValue.serverTimestamp(),
-    // });
-
-    // // Wait for import completion
-    // await waitForImportCompletion(operation.name || '');
-
-    // await importDoc.set({
-    //   id,
-    //   status: 'Initial backup restored, replaying final updates...',
-    //   operation: operation.name,
-    //   timestamp: FieldValue.serverTimestamp(),
-    // });
-
     // Run DataFLow updates
     await launchJob(timestamp);
-
-    // await importDoc.set({
-    //   id,
-    //   status: 'Completed',
-    //   operation: operation.name,
-    //   timestamp: FieldValue.serverTimestamp(),
-    // });
   } catch (ex: any) {
     logger.error('Error restoring backup', ex);
-
-    // await db.doc(config.backupDoc).collection('exports').add({
-    //   error: ex.message,
-    //   status: 'Failed',
-    //   timestamp: FieldValue.serverTimestamp(),
-    // });
 
     return Promise.resolve();
   }
