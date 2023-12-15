daa85286dda599438c1f177e46170f04b344f8ec
Jacob Cable <32874567+cabljac@users.noreply.github.com>
2023-12-15 19:48:39 +0000

    feat(*): add gemini extensions (#305)
    Co-authored-by: Mais <mais.alheraki@gmail.com>

---
+++ firestore-multimodal-genai/functions/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+      },
+    ],
+  },
+};
