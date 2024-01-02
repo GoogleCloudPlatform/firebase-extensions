b15a36fc24411ae5ee3f8ff1b7dbb9b0d4d2c0a0
Mais <mais.alheraki@gmail.com>
2024-01-02 12:47:33 +0300

    test: update jest

--- firestore-incremental-capture/functions/jest.config.js
+++ firestore-incremental-capture/functions/jest.config.js
@@ -4,13 +4,27 @@ module.exports = {
   name: packageJson.name,
   displayName: packageJson.name,
   rootDir: './',
+  preset: 'ts-jest',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/__tests__/tsconfig.json',
     },
   },
+  setupFiles: ['<rootDir>/__tests__/jest.setup.ts'],
   testMatch: ['**/__tests__/*.test.ts'],
-  testPathIgnorePatterns: ['manualTesting'],
   testEnvironment: 'node',
-  preset: 'ts-jest',
+  moduleNameMapper: {
+    'firebase-admin/app': '<rootDir>/node_modules/firebase-admin/lib/app',
+    'firebase-admin/eventarc':
+      '<rootDir>/node_modules/firebase-admin/lib/eventarc',
+    'firebase-admin/database':
+      '<rootDir>/node_modules/firebase-admin/lib/database',
+    'firebase-admin/auth': '<rootDir>/node_modules/firebase-admin/lib/auth',
+    'firebase-functions/encoder':
+      '<rootDir>/node_modules/firebase-functions/lib/encoder',
+    'firebase-functions/lib/encoder':
+      '<rootDir>/node_modules/firebase-functions-test/lib/providers/firestore.js',
+    'firebase-admin/firestore':
+      '<rootDir>/node_modules/firebase-functions/lib/v1/providers/firestore.js',
+  },
 };
