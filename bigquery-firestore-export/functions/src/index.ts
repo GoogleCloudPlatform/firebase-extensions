b0e47566909325a0c0a1cd3d605c94b6c3a23e4f
Jacob Cable <jacobcable94@gmail.com>
2025-03-27 10:30:04 +0000

    fix(bigquery-firestore-export): fix other vulnerabilities

--- bigquery-firestore-export/functions/src/index.ts
+++ bigquery-firestore-export/functions/src/index.ts
@@ -15,7 +15,7 @@
  */
 
 import * as admin from 'firebase-admin';
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions/v1';
 import {getExtensions} from 'firebase-admin/extensions';
 
 import * as logs from './logs';
