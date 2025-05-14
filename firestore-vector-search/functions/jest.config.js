942f430f1f70104419a923296a19667f452cb9ef
Jacob Cable <jacobcable94@gmail.com>
2025-05-14 14:12:26 +0100

    chore(firestore-vector-search): format

--- firestore-vector-search/functions/jest.config.js
+++ firestore-vector-search/functions/jest.config.js
@@ -5,20 +5,28 @@ module.exports = {
   testEnvironment: 'node',
   rootDir: './',
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      tsconfig: '<rootDir>/tsconfig.json'
-    }]
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.json',
+      },
+    ],
   },
   setupFiles: ['<rootDir>/__tests__/jest.setup.ts'],
   testMatch: ['**/*.test.ts'],
   moduleNameMapper: {
-    'firebase-admin/eventarc': '<rootDir>/node_modules/firebase-admin/lib/eventarc',
+    'firebase-admin/eventarc':
+      '<rootDir>/node_modules/firebase-admin/lib/eventarc',
     'firebase-admin/auth': '<rootDir>/node_modules/firebase-admin/lib/auth',
     'firebase-admin/app': '<rootDir>/node_modules/firebase-admin/lib/app',
-    'firebase-admin/database': '<rootDir>/node_modules/firebase-admin/lib/database',
-    'firebase-admin/firestore': '<rootDir>/node_modules/firebase-admin/lib/firestore',
-    'firebase-admin/functions': '<rootDir>/node_modules/firebase-admin/lib/functions',
+    'firebase-admin/database':
+      '<rootDir>/node_modules/firebase-admin/lib/database',
+    'firebase-admin/firestore':
+      '<rootDir>/node_modules/firebase-admin/lib/firestore',
+    'firebase-admin/functions':
+      '<rootDir>/node_modules/firebase-admin/lib/functions',
     'firebase-functions/v2': '<rootDir>/node_modules/firebase-functions/lib/v2',
-    'firebase-admin/extensions': '<rootDir>/node_modules/firebase-admin/lib/extensions',
+    'firebase-admin/extensions':
+      '<rootDir>/node_modules/firebase-admin/lib/extensions',
   },
 };
