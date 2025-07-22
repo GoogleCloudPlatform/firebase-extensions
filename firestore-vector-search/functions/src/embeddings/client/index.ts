ca904391bb4c99572dc959074cf36f0b7404c92a
Corie Watson <watson.corie@gmail.com>
2025-07-22 09:47:37 +0100

    fix(firestore-vector-search): backfill embedding model corrected (#728)
    * fix(firestore-vector-search): backfill embedding model corrected
    
    * chore(firestore-vector-search): removing some old stuff
    
    * chore(firestore-vector-search): adjust changelog

--- firestore-vector-search/functions/src/embeddings/client/index.ts
+++ firestore-vector-search/functions/src/embeddings/client/index.ts
@@ -1,30 +1,22 @@
 import {config, EmbeddingProvider} from '../../config';
 import {MultimodalEmbeddingClient} from './multimodal';
 import {CustomEndpointClient} from './text/custom_function';
-import {GeminiAITextEmbedClient} from './text/gemini';
 import {OpenAIEmbedClient} from './text/open_ai';
-import {VertexAITextEmbedClient} from './text/vertex_ai';
 import {GenkitEmbedClient} from './genkit';
 const getEmbeddingClient = () => {
   // Use Genkit where possible.
-  if (
-    //  Note genkit is yet to suppport multimodal embeddings
-    (config.embeddingProvider as string) === 'gemini' ||
-    (config.embeddingProvider as string) === 'vertex'
-  ) {
-    const provider =
-      config.embeddingProvider === 'vertex' ? 'vertexai' : 'googleai';
-
-    return new GenkitEmbedClient({
-      batchSize: 1,
-      dimension: 768,
-      provider,
-    });
-  }
-
   switch (config.embeddingProvider) {
     case 'gemini' as EmbeddingProvider.Gemini:
-      return new GeminiAITextEmbedClient();
+    case 'vertex' as EmbeddingProvider.VertexAI: {
+      // Note genkit is yet to support multimodal embeddings
+      const provider =
+        config.embeddingProvider === 'vertex' ? 'vertexai' : 'googleai';
+      return new GenkitEmbedClient({
+        batchSize: 1,
+        dimension: 768,
+        provider,
+      });
+    }
     case 'multimodal' as EmbeddingProvider.Multimodal:
       return new MultimodalEmbeddingClient({
         batchSize: 1,
@@ -32,8 +24,6 @@ const getEmbeddingClient = () => {
       });
     case 'openai' as EmbeddingProvider.OpenAI:
       return new OpenAIEmbedClient();
-    case 'vertex' as EmbeddingProvider.VertexAI:
-      return new VertexAITextEmbedClient();
     case 'custom' as EmbeddingProvider.Custom:
       return new CustomEndpointClient();
     default:
